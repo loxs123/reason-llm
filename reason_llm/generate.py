987bffacdb9c4438acb0b6d26b17585da94f2508
LiXin <76460924+loxs123@users.noreply.github.com>
2025-04-29 23:30:13 +0800

    remove use_tqdm=False to prevent stuck in vllm

--- reason_llm/generate.py
+++ reason_llm/generate.py
@@ -74,7 +74,7 @@ elif GENERATE_BACKEND == 'vllm':
                 )
                 for p in prompts
             ]
-            outputs = self.llm.generate(formatted_prompts, sampling_params=_sampling_params, use_tqdm=False)
+            outputs = self.llm.generate(formatted_prompts, sampling_params=_sampling_params)
             return [
                 prompt + [{"role": "assistant", "content": output.outputs[0].text}]
                 # prompt + [{"role": "assistant", "content": remove_stutter(output.outputs[0].text)}]
